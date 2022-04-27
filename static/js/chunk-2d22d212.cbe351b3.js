a1c3761a95e422dec91800b444b979e6d68a5d9d
Tutu <76162631+Tutu-NJR10byh@users.noreply.github.com>
2022-04-27 22:42:02 +0800

    文件管理新增生成个人所得税备案表

---
+++ static/js/chunk-2d22d212.cbe351b3.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d212"],{f5d1:function(e,n,c){"use strict";c.r(n);var t=function(){var e=this,n=e.$createElement,c=e._self._c||n;return c("div")},a=[],d={name:"licence_contract_detail"},i=d,l=c("2877"),r=Object(l["a"])(i,t,a,!1,null,"1c48d486",null);n["default"]=r.exports}}]);
